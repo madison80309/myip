db0f15f85e5feb967b709e2e0301b7fca9902bb0
Madison <133603530+madison80309@users.noreply.github.com>
2025-05-15 09:12:33 +0800

    Update ip_extractor.py

--- ip_extractor.py
+++ ip_extractor.py
@@ -141,7 +141,7 @@ def main():
         if domain:  # 如果是空行则跳过
             print(f"正在处理域名: {domain}")
             # 直接使用域名构造 URL，不需要加前缀
-            url = f'https://www.nslookup.io/domains/{domain}/dns-records/#cloudflare'
+            url = f'https://www.nslookup.io/domains/{domain}/dns-records/#cloudflare'，'https://www.itdog.cn/tcping/ips.meizitu.net:443'
             ips = get_ips_from_url(url, driver)
             ip_addresses.update(ips)  # 将提取的IP地址添加到集合中
 
